489dd6fac1de6cb745c1810abbbcf825c30bdb2a
Juan Garcia <150711485+Wanakox@users.noreply.github.com>
2026-08-19 14:08:28 +0200

    feat: add homepage and authentication pages

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 
 import { HomePage } from '../pages/HomePage'
+import { LoginPage } from '../pages/LoginPage'
+import { RegisterPage } from '../pages/RegisterPage'
 
 export function AppRouter() {
   return (
     <Routes>
       <Route element={<HomePage />} path="/" />
+      <Route element={<RegisterPage />} path="/registro" />
+      <Route element={<LoginPage />} path="/iniciar-sesion" />
     </Routes>
   )
 }
